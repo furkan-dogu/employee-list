7b332305d72aee3a4e5f0d213c731aecbec95094
Furkan Doğu <furkandogu2018@gmail.com>
2023-12-05 11:10:38 +0300

    first commit

---
+++ src/components/List.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+const List = ({data}) => {
+  return (
+
+    <>
+    {data.map((item)=>(
+
+      <article className="person" key={item.id}>
+      <img src={item.image} alt={item.name} />
+      <div>
+        <h4>{item.name}</h4>
+        <p>{item.email}</p>
+        <p>{item.age} years</p>
+      </div>
+    </article>
+
+    ))}
+      
+    </>
+  );
+};
+
+export default List;
